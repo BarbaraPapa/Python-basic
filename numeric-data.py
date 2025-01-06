b084c08132ad8c01c3d249647bd8dd97404e2a58
BarbaraPapa <barbara.papa.d@gmail.com>
2025-01-06 15:16:23 +0000

    first commit labs 108 109 110 111 112

---
+++ numeric-data.py
@@ -0,0 +1,32 @@
+# Exercise 1
+print("Python has three numeric types: int, float, and complex")
+
+# Exercise 2  Creating a variable
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + "is of the data type " + str(type(myValue)))
+
+# Exercise 3  float data type
+myValue=3.14
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercise 4  complex data type
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+# Exercise 5  boolean data type
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
